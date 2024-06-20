776349d7ce4bc534f3cad1e512da50aaf6e05ff4
Marcel <marcelbeldean@outlook.com>
2024-06-20 17:05:47 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +14,9 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +31,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try {
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch(Exception ex) {
+    logger.LogError(ex, "An error occured during migration");
+}
+
+
 app.Run();
 
 
@@ -34,3 +52,11 @@ app.Run();
 // dotnet watch --no-hot-reload
 // dotnet ef migrations add InitialCreate -o Data/Migrations
 // dotnet ef database update
+
+// dotnet ef migrations add "InitialCreate" --project Infrastructure --output-dir Data/Migrations/  --startup-project API 
+// dotnet ef migrations remove --project Infrastructure --startup-project API 
+
+// dotnet ef --project .\ProjectK.Main --startup-project .\ProjectK.Main migrations add "MigrationName"
+// dotnet ef --project .\ProjectK.Main --startup-project .\ProjectK.Main migrations remove
+// dotnet ef --project .\ProjectK.Main --startup-project .\ProjectK.Main database update
+// dotnet ef --project .\ProjectK.Main --startup-project .\ProjectK.Main migrations script --idempotent --output "script.sql" --context SQLDBContext
