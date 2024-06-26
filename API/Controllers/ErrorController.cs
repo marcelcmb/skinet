2c355a18bb77d7f97006a411f656eec9b2e552f3
Marcel <marcelbeldean@outlook.com>
2024-06-26 08:14:49 +0300

    End of section 5

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi =true)] // ignore this in Swagger because of the "ambiguous error"
+    public class ErrorController : BaseApiController
+    {
+
+        // this is for {{url}}/api/endpointthatdoesnotexist
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
-using Core.Interfaces;
+using API.Extensions;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,21 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+// this is for {{url}}/api/endpointthatdoesnotexist
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -39,11 +36,13 @@ var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 
-try {
+try
+{
     await context.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
 }
-catch(Exception ex) {
+catch (Exception ex)
+{
     logger.LogError(ex, "An error occured during migration");
 }
 
